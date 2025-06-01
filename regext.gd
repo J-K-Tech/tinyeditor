d90be4029acb07910f5dbb328da08f24bb162556
komi <jmmfcbb@outlook.com>
2025-06-01 03:34:31 -0300

    update to be compatible with the new tiny version

---
+++ regext.gd
@@ -0,0 +1,7 @@
+class regextmatch extends RegExMatch:
+	var lines:Array[int]
+	pass
+class regext extends RegEx:
+	func search_all_ext(text:String) -> Array[regextmatch]:
+		
+		return []
--- syntaxregex.gd
+++ syntaxregex.gd
@@ -5,6 +5,7 @@ func _get_line_syntax_highlighting(line: int) -> Dictionary:
 	var editor=get_text_edit()
 	var text=editor.get_line(line)
 	var high={}
+	high[0]={"color":Color.WHITE}
 	if text.begins_with(';'):
 		var color=Color.GREEN
 		var charasterisk=text.to_utf8_buffer()
@@ -24,13 +25,20 @@ func _get_line_syntax_highlighting(line: int) -> Dictionary:
 			high[eqpos]={"color":Color.RED}
 			high[eqpos+1]={"color":Color.MAGENTA}
 		return high
+	
 	if text.begins_with("%"):
 		high[0]={"color":Color.YELLOW}
 		high[1]={"color":Color.CYAN}
 		high[text.rfind("%")]={"color":Color.YELLOW}
+		high[text.rfind("%")+1]={"color":Color.GREEN}
 		return high
 	if text.begins_with("\\n"):
 		high[0]={"color":Color.YELLOW}
 		high[2]={"color":Color.GREEN}
+		return high;
+	if text.begins_with("\\"):
+		high[0]={"color":Color.RED}
+		high[1]={"color":Color.WHITE}
+		
 	return high
 	
